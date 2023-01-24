68d5c2b729db22f5dabc513ba85bf3ec460f03f8
jethro tang <jthtang10@gmail.com>
2023-01-24 08:24:51 +0000

    Initial commit

---
+++ 03 - CSS Variables/index.js
@@ -0,0 +1,19 @@
+// Select all inputs
+const inputs = document.querySelectorAll('.controls input');
+
+// event handler
+const handleUpdate = ({currentTarget}) => {
+  // if the element that triggered the event has a data atrribute sizing store it in a variable
+  const suffix = currentTarget.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${currentTarget.name}`, `${currentTarget.value}${suffix}`);
+}
+
+// function handleUpdate() {
+//   const suffix = this.dataset.sizing || '';
+//   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+// }
+
+inputs.forEach(input => {
+  input.addEventListener('change', handleUpdate)
+  input.addEventListener('mousemove', handleUpdate)
+})
